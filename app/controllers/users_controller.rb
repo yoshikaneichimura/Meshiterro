5d9b517f30571711a08a7a26babcae3e1aa597cf
yoshikaneichimura <yoshikanemobile@gmail.com>
2025-05-12 11:18:36 +0000

    [Fix] users コントローラー

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,8 @@ class UsersController < ApplicationController
     @user.update(user_params)
     redirect_to user_path(@user.id)
   end
-  
-  def user_params
-    params.require(:user).permit(:name, :profile)
 
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 end
