d064a8b6bf7697de285525feb35c34cb83d8bde5
yoshikaneichimura <yoshikanemobile@gmail.com>
2025-05-26 10:24:32 +0000

    [Add] post_comment コメント機能

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -8,7 +8,6 @@ class PostImagesController < ApplicationController
     @post_image.user_id = current_user.id
     @post_image.save
     redirect_to post_images_path
-
   end
 
   def index
@@ -17,13 +16,13 @@ class PostImagesController < ApplicationController
 
   def show
     @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
 
   def destroy
     post_image = PostImage.find(params[:id])
     post_image.destroy
     redirect_to post_images_path
-
   end
 
   private
